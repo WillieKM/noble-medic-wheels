6d9e4b460afe8319866448fe0e94efe290e76ac8
WillieKM <willie@cyber-node.com>
2026-01-03 16:50:44 -0800

    Add main sections to Home component

--- app/page.jsx
+++ app/page.jsx
@@ -1,1 +1,19 @@
+import Hero from "../components/Hero";
+import ServicesSection from "../components/ServicesSection";
+import FleetSection from "../components/FleetSection";
+import BrokerSection from "../components/BrokerSection";
+import ContactSection from "../components/ContactSection";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <ServicesSection />
+      <FleetSection />
+      <BrokerSection />
+      <ContactSection />
+    </>
+  );
+}
+
 
