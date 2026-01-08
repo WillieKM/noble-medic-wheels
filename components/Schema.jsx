33b863b8fa3531e9e5a79944ae963dce7d489c66
WillieKM <willie@cyber-node.com>
2026-01-08 12:56:36 -0800

    Update Schema.jsx

--- components/Schema.jsx
+++ components/Schema.jsx
@@ -1,27 +1,10 @@
-export default function Schema() {
-  const schema = {
-    "@context": "https://schema.org",
-    "@type": "MedicalBusiness",
-    name: "Noble Medic Wheels",
-    url: "https://noblemedicwheels.com",
-    logo: "https://noblemedicwheels.com/logo.png",
-    image: "https://noblemedicwheels.com/og-image.jpg",
-    description:
-      "Safe, reliable Non-Emergency Medical Transportation (NEMT) serving King & Pierce County.",
-    telephone: "+1-253-000-0000",
-    areaServed: [
-      { "@type": "AdministrativeArea", name: "King County, WA" },
-      { "@type": "AdministrativeArea", name: "Pierce County, WA" }
-    ],
-    serviceType: "Non-Emergency Medical Transportation",
-    openingHours: "Mo-Sa 06:00-20:00",
-  };
-
+export default function Schema({ data }) {
   return (
     <script
       type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify(data),
+      }}
     />
   );
 }
-
